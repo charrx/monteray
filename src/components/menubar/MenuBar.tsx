7b0bc3deec046cec5d540f2fcb9ff160da63f84e
charrx <leanygaard97@gmail.com>
2022-08-10 21:05:13 +0200

    refactor: menubar

--- src/components/menubar/MenuBar.tsx
+++ src/components/menubar/MenuBar.tsx
@@ -1,8 +1,9 @@
 import { useReducer } from "react";
 import styles from "./MenuBar.module.scss";
+import svg from "../../assets/images/appleicon.svg";
 
 const MenuBar = () => {
-  const [isHidden, setIsHidden] = useReducer((isHidden) => !isHidden, true);
+  const [isOpen, setIsOpen] = useReducer((isOpen) => !isOpen, false);
 
   const dateAndTime = new Date();
   const date = dateAndTime.toDateString();
@@ -10,14 +11,16 @@ const MenuBar = () => {
 
   return (
     <div className={styles.menubar}>
-      <button onClick={setIsHidden}>Apple</button>
-      <div className={`switch ${isHidden ? styles.hidden : styles.menu}`}>
+      <button onClick={setIsOpen}>
+        <img src={svg} alt="apple logo"></img>
+      </button>
+      <div className={`switch ${isOpen ? styles.menu : styles.hidden}`}>
         <p>About this mac</p>
         <hr></hr>
         <p>Lock screen</p>
       </div>
-      <p className={styles.time}>{date}</p>
-      <p className={styles.time}>{time}</p>
+      {/* <p className={styles.time}>{date}</p>
+      <p className={styles.time}>{time}</p> */}
     </div>
   );
 };
