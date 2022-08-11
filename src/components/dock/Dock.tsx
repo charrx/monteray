6b0afb0e0ddf8a91a6b87adbb5e31a03f51974da
charrx <leanygaard97@gmail.com>
2022-08-11 22:29:54 +0200

    chore: initial dock position

--- src/components/dock/Dock.tsx
+++ src/components/dock/Dock.tsx
@@ -1,7 +1,11 @@
+import styles from "./Dock.module.scss";
+
 const Dock = () => {
   return (
-    <div className="dock">
-      <h1>Hello</h1>
+    <div className={styles.dock_container}>
+      <div className={styles.dock}>
+        <p>hello</p>
+      </div>
     </div>
   );
 };
