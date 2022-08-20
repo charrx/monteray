c31b18e03004ddbfac3aa5bcd3634fa581ca80aa
charrx <leanygaard97@gmail.com>
2022-08-20 22:29:58 +0200

    chore: creates desktop and lock screen component and places them in context folder

---
+++ src/components/Context/Desktop.tsx
@@ -0,0 +1,13 @@
+import Dock from "../Dock/Dock";
+import MenuBar from "../menubar/MenuBar";
+
+const Desktop = () => {
+  return (
+    <div>
+      <MenuBar></MenuBar>
+      <Dock></Dock>
+    </div>
+  );
+};
+
+export default Desktop;
---
+++ src/components/Context/LockScreen.tsx
@@ -0,0 +1,9 @@
+const LockScreen = () => {
+  return (
+    <div className="lock-screen">
+      <h1>Lock Screen</h1>
+    </div>
+  );
+};
+
+export default LockScreen;
