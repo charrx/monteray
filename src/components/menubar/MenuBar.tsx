4cf005b8ddc4b0656c3f6f01c54abc5030636163
charrx <leanygaard97@gmail.com>
2022-08-20 03:42:47 +0200

    chore: uses menu component in the menu bar and cleans up code

--- src/components/menubar/MenuBar.tsx
+++ src/components/menubar/MenuBar.tsx
@@ -1,26 +1,19 @@
 import { useReducer } from "react";
 import styles from "./MenuBar.module.scss";
 import svg from "../../assets/images/appleicon.svg";
+import Menu from "../menu/Menu";
 
 const MenuBar = () => {
   const [isOpen, setIsOpen] = useReducer((isOpen) => !isOpen, false);
-
-  const dateAndTime = new Date();
-  const date = dateAndTime.toDateString();
-  const time = dateAndTime.toLocaleTimeString();
 
   return (
     <div className={styles.menubar}>
       <button className={styles.icon} onClick={setIsOpen}>
         <img src={svg} alt="apple logo"></img>
       </button>
-      <div className={`switch ${isOpen ? styles.menu : styles.hidden}`}>
-        <p>About this mac</p>
-        <hr></hr>
-        <p>Lock screen</p>
-      </div>
-      {/* <p className={styles.time}>{date}</p>
-      <p className={styles.time}>{time}</p> */}
+
+      {/* implement menu component */}
+      <Menu isOpen={isOpen} />
     </div>
   );
 };
