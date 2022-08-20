d8e0c0a98d53a6b4df51886993b039965ca4dd38
charrx <leanygaard97@gmail.com>
2022-08-20 03:42:01 +0200

    chore: uses the dockitem component and changes styles from module out with tailwind

--- src/components/dock/Dock.tsx
+++ src/components/dock/Dock.tsx
@@ -1,10 +1,11 @@
-import styles from "./Dock.module.scss";
+import trashcan from "../../assets/images/trashcan.svg";
+import DockItem from "./DockItem";
 
 const Dock = () => {
   return (
-    <div className={styles.dock_container}>
-      <div className={styles.dock}>
-        <p>hello</p>
+    <div className="flex w-full content-end justify-center absolute bottom-0 m-6">
+      <div className="bg-white bg-opacity-40 rounded-2xl p-2 border-solid border-1 border-white border-opacity-50">
+        <DockItem icon={trashcan} />
       </div>
     </div>
   );
