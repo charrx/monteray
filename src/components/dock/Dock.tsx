65aae1e313ea09d5faa212777701ac84eb88deda
charrx <leanygaard97@gmail.com>
2022-08-20 22:30:18 +0200

    chore: adds spotify dock item

--- src/components/dock/Dock.tsx
+++ src/components/dock/Dock.tsx
@@ -1,10 +1,12 @@
 import trashcan from "../../assets/images/trashcan.svg";
+import spotify from "../../assets/images/spotify.svg";
 import DockItem from "./DockItem";
 
 const Dock = () => {
   return (
-    <div className="flex w-full content-end justify-center absolute bottom-0 m-6">
-      <div className="bg-white bg-opacity-40 rounded-2xl p-2 border-solid border-1 border-white border-opacity-50">
+    <div className="flex w-full content-end justify-center absolute bottom-0 mb-2 m-6">
+      <div className="flex flex-row bg-white bg-opacity-40 rounded-2xl p-2 border-solid border-1 border-white border-opacity-50">
+        <DockItem icon={spotify} />
         <DockItem icon={trashcan} />
       </div>
     </div>
