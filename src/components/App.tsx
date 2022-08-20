3d4ae065b9772727a97e5813e894d96fbcbf5916
charrx <leanygaard97@gmail.com>
2022-08-20 22:30:44 +0200

    chore: moves app component to components folder and uses desktop component in app

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import Desktop from "./Context/Desktop";
+
+function App() {
+  return (
+    <div className="w-full h-full bg-[url('./assets/images/pinkwallpaper.jpg')] bg-center bg-no-repeat bg-cover overflow-hidden relative">
+      <Desktop></Desktop>
+    </div>
+  );
+}
+
+export default App;
