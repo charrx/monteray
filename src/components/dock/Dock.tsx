b21a6f98cc4f0f37f166d4d27f23eceadbf7e256
charrx <leanygaard97@gmail.com>
2022-08-21 03:53:09 +0200

    chore: adds finder svg and uses it in the dock component

--- src/components/dock/Dock.tsx
+++ src/components/dock/Dock.tsx
@@ -1,11 +1,13 @@
 import trashcan from "../../assets/images/trashcan.svg";
 import spotify from "../../assets/images/spotify.svg";
+import finder from "../../assets/images/finder.svg";
 import DockItem from "./DockItem";
 
 const Dock = () => {
   return (
     <div className="flex w-full content-end justify-center absolute bottom-0 mb-2 m-6">
       <div className="flex flex-row bg-white bg-opacity-40 rounded-2xl p-2 border-solid border-1 border-white border-opacity-50">
+        <DockItem icon={finder} />
         <DockItem icon={spotify} />
         <DockItem icon={trashcan} />
       </div>
