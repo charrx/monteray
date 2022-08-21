c5f452ecc57292244bb189dd4a365c543f1e0240
charrx <leanygaard97@gmail.com>
2022-08-21 04:30:39 +0200

    chore: refactor lockscreen logic

--- src/components/Context/LockScreen.tsx
+++ src/components/Context/LockScreen.tsx
@@ -1,24 +1,44 @@
+import { useState } from "react";
+import desktop from "../../assets/images/desktop.jpg";
+
 interface Props {
   handleAppContext: () => void;
 }
 
 const LockScreen = ({ handleAppContext }: Props) => {
-  const adminPassword = "admin";
+  const adminPassword = "admin"; // This can be changes to whatever you want.
+  const [password, setPassword] = useState("");
+  const [isInvalidForm, setIsInvalidForm] = useState(false);
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
-    if (value === adminPassword) {
+    setPassword(value);
+  };
+
+  const handleFormValidation = () => {};
+
+  const submit = (e: React.FormEvent<HTMLFormElement>) => {
+    if (password === adminPassword) {
       handleAppContext();
     }
   };
 
   return (
-    <div className="w-full h-full flex justify-center bg-[url('./assets/images/lockscreen.jpg')] bg-center bg-no-repeat bg-cover overflow-hidden">
-      <input
-        className="flex self-center"
-        type="text"
-        onChange={handleOnChange}
-      ></input>
+    <div className="w-full h-full flex flex-col gap-4 items-center justify-center bg-[url('./assets/images/lockscreen.jpg')] bg-center bg-no-repeat bg-cover overflow-hidden">
+      <img
+        className="w-40 h-40 rounded-full"
+        src={desktop}
+        alt="user icon"
+      ></img>
+      <form onSubmit={submit}>
+        <input
+          className=" w-50 h-8 flex rounded-2xl text-center"
+          type="text"
+          onChange={handleOnChange}
+          placeholder="Enter Password"
+        ></input>
+        <button>Log in</button>
+      </form>
     </div>
   );
 };
