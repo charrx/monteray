c5f7365d37b771a3d5a3f800c0dc8898262fab6e
charrx <leanygaard97@gmail.com>
2022-08-26 20:46:31 +0200

    chore: converts desktop component back to scss modules files from tailwind

--- src/components/Context/Desktop.tsx
+++ src/components/Context/Desktop.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Dock from "../Dock/Dock";
 import Finder from "../Finder/Finder";
 import MenuBar from "../Menubar/MenuBar";
+import styles from "../../styles.Desktop.module.scss";
 
 interface Props {
   handleAppContext: () => void;
@@ -15,7 +16,7 @@ const Desktop = ({ handleAppContext }: Props) => {
   };
 
   return (
-    <div className="w-full h-full bg-[url('./assets/images/desktop.jpg')] bg-center bg-no-repeat bg-cover overflow-hidden relative">
+    <div className={styles.desktop_container}>
       <MenuBar handleAppContext={handleAppContext}></MenuBar>
       {isFinderOpen && <Finder />}
       <Dock handleOpenFinder={handleOpenFinder}></Dock>
