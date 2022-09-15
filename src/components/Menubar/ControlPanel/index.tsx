f412a5d4ba7c8f397cc289beee6522bccd896802
charrx <leanygaard97@gmail.com>
2022-09-15 23:25:52 +0200

    chore: improvements to the control panel functionality with matching styles

--- src/components/Menubar/ControlPanel/index.tsx
+++ src/components/Menubar/ControlPanel/index.tsx
@@ -1,5 +1,9 @@
 import Option, { OptionProps, OptionType } from "./Option";
 import styles from "../../../styles/ControlPanel.module.scss";
+import CircularButton from "./CircularButton";
+import { useReducer } from "react";
+import ScreenMirroring from "../Icons/ScreenMirroring";
+import Brightness from "../Icons/Brightness";
 
 const OptionsList: OptionProps[] = [
   {
@@ -23,9 +27,16 @@ const OptionsList: OptionProps[] = [
 ];
 
 const ControlPanel = () => {
+  const [isDarkMode, setIsDarkMode] = useReducer(
+    (isDarkMode: boolean) => !isDarkMode,
+    false
+  );
+
   return (
-    <div className={styles.menu}>
-      <div className={styles.hello}>
+    <div className={styles.controlpanel_menu}>
+      <div
+        className={`${styles.left_side_container} ${styles.option_container}`}
+      >
         {OptionsList.map((option) => {
           return (
             <div key={option.type}>
@@ -34,14 +45,32 @@ const ControlPanel = () => {
           );
         })}
       </div>
-      <div className={styles.hello}>
-        {OptionsList.map((option) => {
-          return (
-            <div key={option.type}>
-              <Option {...option} />
+      <div className={styles.right_side_container}>
+        <div
+          className={`${styles.darkmode_container} ${styles.option_container}`}
+          onClick={setIsDarkMode}
+        >
+          <CircularButton type={OptionType.DARKMODE} isActive={!isDarkMode} />
+          <p>{isDarkMode ? "Dark Mode" : "Light Mode"}</p>
+        </div>
+        <div className={styles.utilities_container}>
+          <div
+            className={`${styles.readonly_options} ${styles.option_container}`}
+          >
+            <div className={styles.icon_container}>
+              <Brightness />
             </div>
-          );
-        })}
+            <p>Keyboard Brightness</p>
+          </div>
+          <div
+            className={`${styles.readonly_options} ${styles.option_container}`}
+          >
+            <div className={styles.icon_container}>
+              <ScreenMirroring />
+            </div>
+            <p>Screen Mirroring</p>
+          </div>
+        </div>
       </div>
     </div>
   );
