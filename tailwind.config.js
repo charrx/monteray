c55c76efdc798346d46c017eedd9a11c7cc52031
charrx <leanygaard97@gmail.com>
2023-08-26 20:28:48 +0200

    chore: extends theme with new colors for the finder window

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        whiteish: "rgb(var(--color-whiteish) / <alpha-value>)",
+        windowBtnClose: "rgba(255,94,87,255)",
+        windowBtnMinimize: "rgba(255,188,46,255)",
+        windowBtnMaximize: "rgba(40,200,64,255)",
+        windowBackground: "rgb(225,225,225)",
       },
       padding: {
         2.2: "0.2rem",
