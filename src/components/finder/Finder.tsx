78feca84b9c19c65a2b298f706627d817c652f5e
charrx <leanygaard97@gmail.com>
2023-08-27 00:45:35 +0200

    chore: refactor finder component to use tailwind

--- src/components/finder/Finder.tsx
+++ src/components/finder/Finder.tsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import Draggable from "react-draggable";
-import styles from "../../styles/Finder.module.scss";
 import ActionButtons from "../ActionButtons";
 
 interface Props {
@@ -11,12 +10,15 @@ const Finder = ({ closeWindowHandler }: Props) => {
   //   If running in React Strict mode, ReactDOM.findDOMNode() is deprecated.
   //   Unfortunately, in order for <Draggable> to work properly, we need raw access
   //   to the underlying DOM node. If you want to avoid the warning, pass a `nodeRef`.
-
   const nodeRef = useRef(null);
+
   return (
     <Draggable defaultPosition={{ x: 50, y: 50 }} nodeRef={nodeRef}>
-      <div className={styles.finder_window_container} ref={nodeRef}>
-        <div className={styles.finder_window_header}>
+      <div
+        className="flex w-1/2 h-1/2 bg-white rounded-xl -z-10 shadow-2xl"
+        ref={nodeRef}
+      >
+        <div className="w-36 h-full flex bg-windowBackground rounded-l-xl">
           <ActionButtons closeWindowHandler={closeWindowHandler} />
         </div>
       </div>
