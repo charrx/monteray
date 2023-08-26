2912c583ffe7f0a42d5e078ff258d48562e1f024
charrx <leanygaard97@gmail.com>
2023-08-26 20:23:57 +0200

    chore: refactor actionsbuttons to use tailwind

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -1,18 +1,16 @@
-import styles from "../styles/ActionButtons.module.scss";
-
 interface Props {
   closeWindowHandler: () => void;
 }
 
 const ActionButtons = ({ closeWindowHandler }: Props) => {
   return (
-    <div className={styles.action_buttons_container}>
+    <div className="flex gap-2">
       <button
-        className={`${styles.action_button} ${styles.close}`}
+        className="w-3 h-3 rounded-full border border-solid border-black/10 box-border bg-windowBtnClose"
         onClick={closeWindowHandler}
       ></button>
-      <button className={`${styles.action_button} ${styles.minimize}`}></button>
-      <button className={`${styles.action_button} ${styles.expand}`}></button>
+      <button className="w-3 h-3 rounded-full border border-solid border-black/10 box-border bg-windowBtnMinimize"></button>
+      <button className="w-3 h-3 rounded-full border border-solid border-black/10 box-border bg-windowBtnMaximize"></button>
     </div>
   );
 };
