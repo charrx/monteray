af36bb3ea30193c76550d8d99fbde1ed9d773c31
charrx <leanygaard97@gmail.com>
2023-09-02 02:18:43 +0200

    chore: add custom fontsize and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ export default {
         windowBtnMinimize: "rgba(255,188,46,255)",
         windowBtnMaximize: "rgba(40,200,64,255)",
         windowBackground: "rgb(225,225,225)",
+        highlighter: "rgb(75,145,247)",
       },
       padding: {
         2.2: "0.2rem",
+      },
+      fontSize: {
+        xxs: ["0.625rem", "0.75rem"],
       },
       backgroundImage: {
         "mac-desktop": "url('./src/assets/images/desktop.jpg')",
