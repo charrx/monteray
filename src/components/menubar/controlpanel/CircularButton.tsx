ff1258ea9d23ffdcea6e6330cacb345d3c82c74d
charrx <leanygaard97@gmail.com>
2023-09-02 02:28:17 +0200

    refactor: circularbutton to use tailwind

--- src/components/menubar/controlpanel/CircularButton.tsx
+++ src/components/menubar/controlpanel/CircularButton.tsx
@@ -1,25 +1,37 @@
-import { Airdrop, Wifi, Moon } from "../Icons";
-import styles from "../../../styles/CircularButton.module.scss";
-import { OptionType } from "./Option";
+import { createElement } from "react";
+import { Airdrop, Wifi, Moon } from "../icons";
+import { OptionType } from "./OptionType";
 
 interface Props {
   type: OptionType;
   isActive: boolean;
 }
 
-const CircularButton = ({ type, isActive }: Props) => {
-  const iconRecord: { [key in OptionType]: JSX.Element } = {
-    [OptionType.WIFI]: <Wifi />,
-    [OptionType.BLUETOOTH]: <Wifi />,
-    [OptionType.AIRDROP]: <Airdrop />,
-    [OptionType.DARKMODE]: <Moon />,
-  };
+type IconType = typeof Wifi | typeof Airdrop | typeof Moon;
 
+const iconRecord: { [key in OptionType]: IconType } = {
+  [OptionType.WIFI]: Wifi,
+  [OptionType.BLUETOOTH]: Wifi,
+  [OptionType.AIRDROP]: Airdrop,
+  [OptionType.DARKMODE]: Moon,
+};
+
+// .circular_button svg path {
+//     fill: $black;
+// }
+
+// .active svg path {
+//     fill: $white;
+// }
+
+const CircularButton = ({ type, isActive }: Props) => {
   return (
     <button
-      className={`${styles.circular_button} ${isActive ? styles.active : ""}`}
+      className={`flex w-7 h-7 rounded-full justify-center items-center ${
+        isActive ? "bg-highlighter" : "bg-windowBackground"
+      }`}
     >
-      {iconRecord[type]}
+      {createElement(iconRecord[type])}
     </button>
   );
 };
